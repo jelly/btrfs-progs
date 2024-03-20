c76bff0b5ed35a429a136c47d41e6cc37402bb8c
Jelle van der Waa <jvanderwaa@redhat.com>
2024-03-20 20:37:44 +0100

    btrfs-progs: filesystem show: introduce printing helper functions
    Introduce helper functions for printing the filesystem data and list of
    devices. This prepares the both printing code paths of filesystem show
    to support JSON output.
    
    This introduces an inconsistency with the old version where a missing
    device would now use pretty_size_mode instead of printing 0:
    
    devid    1 size 0 used 0 path /dev/sdb MISSING
    
    to:
    
    devid    1 size 0.00B used 0.00B path /dev/sdb
    
    Signed-off-by: Jelle van der Waa <jvanderwaa@redhat.com>

--- cmds/filesystem.c
+++ cmds/filesystem.c
@@ -296,6 +296,33 @@ static void splice_device_list(struct list_head *seed_devices,
 	list_splice(seed_devices, all_devices);
 }
 
+static void print_filesystem_info(char *label, char uuidbuf[BTRFS_UUID_UNPARSED_SIZE],
+				u64 bytes_used, u64 num_devices,
+				unsigned unit_mode)
+{
+	if (label)
+		pr_default("Label: '%s' ", label);
+	else
+		pr_default("Label: none ");
+
+	pr_default(" uuid: %s\n\tTotal devices %llu FS bytes used %s\n", uuidbuf,
+			num_devices,
+			pretty_size_mode(bytes_used, unit_mode));
+}
+
+static void print_filesystem_device(u64 devid, u64 total_bytes, u64 bytes_used,
+				char *path,
+				bool missing,
+				unsigned unit_mode)
+{
+	pr_default("\tdevid %4llu size %s used %s path %s%s\n",
+	       devid,
+	       pretty_size_mode(total_bytes, unit_mode),
+	       pretty_size_mode(bytes_used, unit_mode),
+	       path,
+	       missing ? " MISSING":  "");
+}
+
 static void print_devices(struct btrfs_fs_devices *fs_devices,
 			  u64 *devs_found, unsigned unit_mode)
 {
@@ -313,12 +340,11 @@ static void print_devices(struct btrfs_fs_devices *fs_devices,
 
 	list_sort(NULL, all_devices, cmp_device_id);
 	list_for_each_entry(device, all_devices, dev_list) {
-		pr_default("\tdevid %4llu size %s used %s path %s\n",
-		       device->devid,
-		       pretty_size_mode(device->total_bytes, unit_mode),
-		       pretty_size_mode(device->bytes_used, unit_mode),
-		       device->name);
-
+		print_filesystem_device(device->devid,
+					device->total_bytes, device->bytes_used,
+					device->name,
+					false,
+					unit_mode);
 		(*devs_found)++;
 	}
 }
@@ -337,14 +363,11 @@ static void print_one_uuid(struct btrfs_fs_devices *fs_devices,
 	uuid_unparse(fs_devices->fsid, uuidbuf);
 	device = list_entry(fs_devices->devices.next, struct btrfs_device,
 			    dev_list);
-	if (device->label && device->label[0])
-		pr_default("Label: '%s' ", device->label);
-	else
-		pr_default("Label: none ");
-
 	total = device->total_devs;
-	pr_default(" uuid: %s\n\tTotal devices %llu FS bytes used %s\n", uuidbuf,
-	       total, pretty_size_mode(device->super_bytes_used, unit_mode));
+
+	print_filesystem_info(device->label && device->label[0] ? device->label : NULL, uuidbuf,
+			      device->super_bytes_used, total,
+			      unit_mode);
 
 	print_devices(fs_devices, &devs_found, unit_mode);
 
@@ -381,15 +404,9 @@ static int print_one_fs(struct btrfs_ioctl_fs_info_args *fs_info,
 		return ret;
 
 	uuid_unparse(fs_info->fsid, uuidbuf);
-	if (label && *label)
-		pr_default("Label: '%s' ", label);
-	else
-		pr_default("Label: none ");
-
-	pr_default(" uuid: %s\n\tTotal devices %llu FS bytes used %s\n", uuidbuf,
-			fs_info->num_devices,
-			pretty_size_mode(calc_used_bytes(space_info),
-					 unit_mode));
+	print_filesystem_info(label && *label ? label : NULL, uuidbuf,
+				calc_used_bytes(space_info), fs_info->num_devices,
+				unit_mode);
 
 	for (i = 0; i < fs_info->num_devices; i++) {
 		char *canonical_path, *path;
@@ -399,10 +416,12 @@ static int print_one_fs(struct btrfs_ioctl_fs_info_args *fs_info,
 		/* Add check for missing devices even mounted */
 		fd = open((char *)tmp_dev_info->path, O_RDONLY);
 		if (fd < 0) {
-			pr_default("\tdevid %4llu size 0 used 0 path %s MISSING\n",
-					tmp_dev_info->devid, tmp_dev_info->path);
+			print_filesystem_device(tmp_dev_info->devid,
+						0, 0,
+						(char *)tmp_dev_info->path,
+						true,
+						unit_mode);
 			continue;
-
 		}
 		close(fd);
 		canonical_path = path_canonicalize((char *)tmp_dev_info->path);
@@ -410,10 +429,11 @@ static int print_one_fs(struct btrfs_ioctl_fs_info_args *fs_info,
 			path = (char *)tmp_dev_info->path;
 		else
 			path = canonical_path;
-		pr_default("\tdevid %4llu size %s used %s path %s\n",
-			tmp_dev_info->devid,
-			pretty_size_mode(tmp_dev_info->total_bytes, unit_mode),
-			pretty_size_mode(tmp_dev_info->bytes_used, unit_mode), path);
+		print_filesystem_device(tmp_dev_info->devid,
+					tmp_dev_info->total_bytes, tmp_dev_info->bytes_used,
+					path,
+					false,
+					unit_mode);
 
 		free(canonical_path);
 	}
